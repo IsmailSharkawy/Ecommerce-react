3f29072057746d37027a3ec6209c588aa3caf4b4
Ismail El-Sharkawy <ismail@aucegypt.edu>
2020-10-13 20:49:10 +0200

    Added Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
-  return (
-    
-    <>
-      <h1>Welcome</h1>
-    </>
-
-
-  );
-}
+	return (
+		<>
+			<Header />
+			<Container>
+				<main className='py-3'>
+					<h1>Welcome To LimitleShop</h1>
+				</main>
+			</Container>
+			<Footer />
+		</>
+	);
+};
 
 export default App;
