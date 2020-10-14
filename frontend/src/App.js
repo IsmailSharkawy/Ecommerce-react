c432c2f56f554a4cc2f6dc73b2e6af78b79735c7
Ismail El-Sharkawy <ismail@aucegypt.edu>
2020-10-14 02:05:19 +0200

    Added product page and displayed products on homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Homescreen from './screens/Homescreen';
+import Productscreen from './screens/Productscreen';
 
 const App = () => {
 	return (
-		<>
+		<Router>
 			<Header />
-			<Container>
-				<main className='py-3'>
-					<h1>Welcome To LimitleShop</h1>
-				</main>
-			</Container>
+			<main className='py-3'>
+				<Container>
+					<Route path='/' component={Homescreen} exact />
+					{/* match.params?? */}
+					<Route path='/product/:id' component={Productscreen} />
+				</Container>
+			</main>
 			<Footer />
-		</>
+		</Router>
 	);
 };
 
